f215dd3f7e85ff167fd50b4190638db095645d05
Alex Sanchez <alesanchez728@gmail.com>
2017-11-14 20:33:57 -0600

    images showing

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -3,8 +3,14 @@
 //app.run() runs one time/ application, AFTER the config
 	// the time to add variables
 
-app.run(function (FIREABASE_CONFIG){
+app.run(function ($rootScope, FIREABASE_CONFIG, tmdbService){
 	firebase.initializeApp(FIREABASE_CONFIG);
+
+	tmdbService.tmdbConfiguration().then((results) => {
+		$rootScope.image_url = results.data.images.base_url;
+	}).catch((err) => {
+		console.log("error in tmdbCondifuration", err);
+	});
 });
 
 app.config(function($routeProvider){
--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,15 +1,16 @@
 'use strict';
 
 app.controller("SearchCtrl", function ($scope, tmdbService) {
+  $scope.movies = [];
 
   $scope.enterPush = (event) => {
     if(event.keyCode === 13) {
       tmdbService.searchMovies(event.target.value).then((results) => {
-        console.log("Movies?", results.data.results);
+        $scope.movies= results.data.results;
       }).catch((err) => {
         console.log('error in searchMovies', err);
       });
   
-    }
-  };
-  });
+      }
+    }; // end enterPush
+  }); // end app controller
