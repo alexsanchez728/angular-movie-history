35e811ace910de52dfdc3987f374767e13cd7d78
Alex Sanchez <alesanchez728@gmail.com>
2017-11-15 19:01:18 -0600

    logout closes #25

--- javascripts/services/AuthServices.js
+++ javascripts/services/AuthServices.js
@@ -1,10 +1,23 @@
 'use strict';
 
-app.service("AuthService", function(){
+app.service("AuthServices", function(){
   const authenticateGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     return firebase.auth().signInWithPopup(provider);
   };
 
-  return {authenticateGoogle};
+  const isAuthenticated = () => {
+    return firebase.auth().currentUser ? true : false;
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+  };
+
+  return { 
+    authenticateGoogle,
+    isAuthenticated,
+    logout
+  };
+  
 });
